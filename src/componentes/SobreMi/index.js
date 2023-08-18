53c94d5fdf46abf392f46120fabfb4a08ae86736
Cristian Fontanini <cristianfonta16@gmail.com>
2023-08-18 08:55:15 -0300

    Mejorando title

--- src/componentes/SobreMi/index.js
+++ src/componentes/SobreMi/index.js
@@ -1,11 +1,14 @@
 import styled  from "styled-components"
 import Foto_cv from "../../assets/images/foto_cv_redonda.png"
 import { Link } from "react-router-dom"
+import FotoGitHub from "../../assets/images/github.png"
+import FotoLinkedIn from "../../assets/images/linkedin.png"
 
 
 export const CajaPrincipal = styled.div`
     width: 90%;
     margin: auto;
+    margin-top: 3rem;
 `
 
 const CajaFotoH3 = styled.div`
@@ -34,22 +37,25 @@ const SobreMi = () => {
                     <img src={Foto_cv} alt="Foto de Cristian Fontanini"/>
                 </div>
                 <div>
-                    <h2>Hola, mi nombre es Cristian Fontanini, construyo páginas web y soy bombero.</h2>
-                    <TextoDescripcion>Me formé en Marketing Digital en Google Activate, Diseño de Páginas Web con Wordpress, Desarrollador FRONT-END en Alura y Argentina Programa 4.0. También soy bombero de experiencia con conocimiento en manejo de camiones, bombas e incendio en general, rescatista en situaciones de magnitud (ej. edificio que se derrumbó en calle Salta 2141, incluso poseo Diploma de Honor de la República Argentina).</TextoDescripcion>
-                </div>
-                <div>
-                    <Link to="https://www.linkedin.com/in/cristian-fontanini/">LinkedIn</Link>
-                    <Link to="https://github.com/Cfonta1986">GitHub</Link>
+                    <h1>Hola!!! Mi nombre es Cristián Fontanini.</h1>
+                    <TextoDescripcion>Construyo PAGINAS WEB, me encanta la DATA SCIENCE y soy bombero. Me formé en Marketing Digital en Google Activate, Diseño de Páginas Web con Wordpress, Desarrollador FRONT-END y Python para Data Science en Alura / Oracle y Argentina Programa 4.0. </TextoDescripcion>
+                     <div>
+                    <Link to="https://www.linkedin.com/in/cristian-fontanini/"><img src={FotoLinkedIn} alt="Foto LikedIn"/></Link>
+                    <Link to="https://github.com/Cfonta1986"><img src={FotoGitHub} alt="Foto GitHub"/></Link>
                     <Link to="#">Curriculum</Link>
                 </div>
+                </div>
+               
             </CajaFotoH3>
             <CajaSobre>
                 <H2Descripcion>Sobre mi</H2Descripcion>
-                <TextoDescripcion>Tengo 36 años, vivo en la ciudad de Rosario,  Argentina.
+                <TextoDescripcion>Con 36 años de edad y residencia en la ciudad de Rosario, Argentina, soy un apasionado por el emprendimiento y con una visión de negocio excepcional. Me siento atraído por los desafíos y poseo una naturaleza detallista y autodidacta que me impulsa constantemente a buscar oportunidades para el crecimiento personal y profesional.
 
-                    Soy una persona emprendedora, con mucha visión de negocio, que le encantan los retos, detallista y autodidacta, en lo posible trato de nunca dejar de aprender.
+                Mi enfoque principal está en el ámbito del Desarrollo Web y Data Science. Considero que el aprendizaje continuo es esencial y siempre estoy en busca de nuevas formas de expandir mi conjunto de habilidades. Mi objetivo primordial es contribuir y prosperar en un entorno laboral que fomente el desarrollo de proyectos innovadores y que esté alineado con mis valores y aspiraciones.
 
-                    Mi objetivo principal es desarrollarme profesionalmente en el sector del Desarrollo Web, es por eso que busco oportunidades que me permitan hacerlo y espero poder alinearme con los objetivos de la empresa que me contrate.</TextoDescripcion>
+                Estoy entusiasmado por la posibilidad de unirme a una empresa dinámica donde pueda aportar mi pasión por el desarrollo web y la ciencia de datos. Aspirando a alinearme con los objetivos estratégicos de la organización, espero desplegar mi potencial y contribuir al éxito mutuo.
+
+                Siendo un emprendedor de corazón y un buscador incansable de soluciones, estoy listo para enfrentar nuevos desafíos y crear un impacto significativo en el mundo del desarrollo tecnológico.</TextoDescripcion>
             </CajaSobre>
         </CajaPrincipal>
     )
