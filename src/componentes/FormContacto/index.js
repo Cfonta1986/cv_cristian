37f69e6d91b8b5286e89407d2e171b88c269dba4
Cristian Fontanini <cristianfonta16@gmail.com>
2023-08-24 14:45:14 -0300

    reparanco formContacto

--- src/componentes/FormContacto/index.js
+++ src/componentes/FormContacto/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import styled from "styled-components";
 import Input from '@mui/material/Input';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button'; 
-
+import Button from '@mui/material/Button';
 
 export const CajaForm = styled.form`
   display: flex;
@@ -17,18 +15,16 @@ const NuevoInput = styled(Input)`
   color: ${ ({theme}) => theme.text };
 `
 
-
- const FormContacto =  () => {
+const FormContacto = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
-  console.log(errors);
-  
+
   return (
     <CajaForm onSubmit={handleSubmit(onSubmit)}>
-      <NuevoInput  aria-label="Nombre" type="text" placeholder="Nombre" {...register("Nombre", {required: true, min: 2})} />
-      <NuevoInput  aria-label="E-Mail" type="email" placeholder="E-Mail" {...register("EMail", {required: true})} />
-      <NuevoInput  aria-label="Asunto" type="text" placeholder="Asunto" {...register("Asunto", {required:true, min: 2})} />
-      <TextField aria-label="Mensaje" label="Mensaje" multiline {...register("Mensaje", { required: true })} />
+      <NuevoInput aria-label="Nombre" type="text" placeholder="Nombre" {...register("Nombre", { required: true, min: 2 })} />
+      <NuevoInput aria-label="E-Mail" type="email" placeholder="E-Mail" {...register("EMail", { required: true })} />
+      <NuevoInput aria-label="Asunto" type="text" placeholder="Asunto" {...register("Asunto", { required: true, min: 2 })} />
+      <textarea aria-label="Mensaje" label="Mensaje" multiline {...register("Mensaje", { required: true })} />
 
       <Button type="submit" size="large" variant="contained">Submit</Button>
     </CajaForm>
@@ -36,6 +32,4 @@ const NuevoInput = styled(Input)`
 }
 
 
-export default FormContacto
-
-
+export default FormContacto;
