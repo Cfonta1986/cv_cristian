83c72303b796d7b3068564757ad4d9322099b877
Cristian Fontanini <cristianfonta16@gmail.com>
2023-08-25 08:26:00 -0300

    dandole estilos a formContacto

--- src/componentes/FormContacto/index.js
+++ src/componentes/FormContacto/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import styled from "styled-components";
 import Input from '@mui/material/Input';
-import Button from '@mui/material/Button';
-import { purple } from '@mui/material/colors';
+
 
 export const CajaForm = styled.form`
   display: flex;
   flex-direction: column;
+  width: 25rem;
 `
 
 const NuevoInput = styled(Input)`
@@ -16,10 +16,20 @@ const NuevoInput = styled(Input)`
   color: ${ ({theme}) => theme.text };
 `
 
-const ColorButton = styled.button(({ theme }) => ({
-  color: theme.text ,
-  backgroundColor: theme.inside
-}));
+const ContainedButton = styled.button`
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
 
   const FormContacto = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -27,12 +37,12 @@ const ColorButton = styled.button(({ theme }) => ({
 
   return (
     <CajaForm onSubmit={handleSubmit(onSubmit)}>
-      <NuevoInput aria-label="Nombre" type="text" placeholder="Nombre" {...register("Nombre", { required: true, min: 2 })} />
-      <NuevoInput aria-label="E-Mail" type="email" placeholder="E-Mail" {...register("EMail", { required: true })} />
-      <NuevoInput aria-label="Asunto" type="text" placeholder="Asunto" {...register("Asunto", { required: true, min: 2 })} />
-      <textarea aria-label="Mensaje" label="Mensaje" placeholder="Mensaje" multiline {...register("Mensaje", { required: true })} />
+      <NuevoInput aria-label="Nombre" type="text" placeholder="Nombre" multiline {...register("Nombre", { required: true, min: 2 })} />
+      <NuevoInput type="email" placeholder="E-Mail" multiline {...register("EMail", { required: true })} />
+      <NuevoInput aria-label="Asunto" type="text" placeholder="Asunto" multiline {...register("Asunto", { required: true, min: 2 })} />
+      <NuevoInput aria-label="Mensaje" label="Mensaje" placeholder="Mensaje" multiline {...register("Mensaje", { required: true })} />
 
-      <ColorButton type="submit" size="large" variant="contained">Enviar</ColorButton>
+      <ContainedButton type="submit" size="large" variant="contained">Enviar</ContainedButton>
     </CajaForm>
   );
 }
